1605b728f62fa4a5fe292c9df3387742d19270af
Nombru <1662220+Nombru@users.noreply.github.com>
2018-09-21 11:53:40 -0400

    Add functionality to the javascript branching and ready the project for CSS and Bootstrap to clean everything up

--- js/scripts.js
+++ js/scripts.js
@@ -4,22 +4,28 @@ $(document).ready(function() {
     var q_OneInput = parseInt($("#q_One").val());
     var q_TwoInput = parseInt($("#q_Two").val());
     var q_ThreeInput = parseInt($("#q_Three").val());
+    var q_FourInput = parseInt($("#q_Four").val());
+    var q_FiveInput = parseInt($("#q_Five").val());
 
     //console.log(q_OneInput);
 
-    var total = q_OneInput + q_TwoInput + q_ThreeInput;
+    var total = q_OneInput + q_TwoInput + q_ThreeInput + q_FourInput + q_FiveInput;
+
     console.log(total);
 
-    if(total === 3){
+    if(total === 5){
       $("#a_One").show();
-    } else if (total === 2) {
+    } else if (total <=8) {
       $("#a_Two").show();
-    } else if (total === 1) {
+    } else if (total === 10) {
+      $("#a_Four").show();
+      $("#a_One").show();
+      $("#a_Two").show();
       $("#a_Three").show();
     } else {
-      $("#a_Four").show();
+      $("#a_Three").show();
     }
-    
+
     event.preventDefault();
   });
 
